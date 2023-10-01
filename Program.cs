86c5ddbafc33c75803ecaf23b0194e6378963e2a
pernl <pernl@MSI>
2023-09-30 23:18:03 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+//interactively parse through the different words in text file by first character etc.
+using System;
+using System.IO;
+
+string[] wordArray = File.ReadAllLines(@"C:\Users\pernl\source\repos\String Program\words.txt");
+
+foreach (var word in wordArray)
+{
+    if(word  == "zoo")
+    {
+        Console.WriteLine(word);
+    }
+}
