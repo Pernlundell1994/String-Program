dd6354078a01e0516d66745abb181aa8bde0e214
Pern Lundell <pernlundell94@gmail.com>
2023-10-01 20:27:33 -0400

    Moved to use certain indices of string array instead of whole array of strings from given text file

--- Program.cs
+++ Program.cs
@@ -1,14 +1,30 @@
 ﻿// See https://aka.ms/new-console-template for more information
-//interactively parse through the different words in text file by first character etc.
+//Interactively parse through certain indices of a string array
+//Beginning with cutting the effective size of the inputted array to a lower amount of indicies
+//The text file being read from has over 400,000 english words
+
 using System;
 using System.IO;
+using System.Collections.Generic;
+using System.Linq;
 
-string[] wordArray = File.ReadAllLines(@"C:\Users\pernl\source\repos\String Program\words.txt");
 
-foreach (var word in wordArray)
+class stringProgram
 {
-    if(word  == "zoo")
-    {
-        Console.WriteLine(word);
+
+    static void Main(string[] args)
+    { 
+    string[] wordArray = File.ReadAllLines(@"C:\Users\pernl\source\repos\String Program\words.txt");
+
+        int halveData = wordArray.Length / 100;
+
+        for(int i = 0; i < halveData; i++)
+        {
+            {
+                Console.WriteLine(wordArray[i]);
+            }
+
+        }
     }
+
 }
